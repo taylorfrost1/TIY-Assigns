f1ba3148653c8c305dbe9604541590f638a770a4
Taylor Frost <taylorhamblinfrost@gmail.com>
2016-06-14 07:59:43 -0600

    Not Done with Agent Swift

--- 15/AgentAppSwift/ViewController.swift
+++ 15/AgentAppSwift/ViewController.swift
@@ -10,14 +10,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var UsernameLabel: UITextField!
+    
+    @IBOutlet weak var PasswordLabel: UITextField!
+    
+    @IBOutlet weak var AuthenticateButton: UIButton!
+    
+ 
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         
         
     }
-
-
-
+    @IBOutlet weak var AuthenticateButtonTapped: UIButton!
+    
+    if UsernameLabel = "Taylor Frost" && PasswordLabel = "111111"{
+    
+    } self.view.backgroundColor = [UIColor greenColor];
+    
+    
+else if (UsernameLabel != "Taylor Frost" && PasswordLabel != "111111") {
+    
+    self.view.backgroundColor = [UIColor redColor];
+    
 }
+
+
+
+
+
 
