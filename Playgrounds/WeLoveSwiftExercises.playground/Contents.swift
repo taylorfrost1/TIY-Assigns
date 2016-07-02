80d0bd34ccad4bd645829de5ae35702f09b7de29
Taylor Frost <taylorhamblinfrost@gmail.com>
2016-07-02 08:02:07 -0600

    Done up to 2.8

---
+++ Playgrounds/WeLoveSwiftExercises.playground/Contents.swift
@@ -0,0 +1,159 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+//1.1
+//var a = 2
+//var b = 3
+//var c = a + b
+
+//1.2
+//var secondsInAYear = 60*60*24*365
+
+//1.3
+//var height = 1920
+//var width = 1080
+//var numberOfPixels = height*width
+
+//1.4
+//let a = 10
+//let b = 6
+//let sum = a + b
+//let difference = a - b
+
+//1.5
+//var width = 8
+//var heigth = 12
+//var x = 2
+//var y = 3
+//var perimeter = width+heigth+x+y
+
+//1.6
+//var a = 1
+//var b = 2
+//var temp = a
+//a = b
+//b = temp
+
+//1.7
+//var a = 123
+//a % 120
+
+//1.8
+//var dogsAge = 50
+//dogsAge/7
+
+//1.9
+//var x = 3
+//var y = 2
+//var bobsAge = 12
+//var alice = 15*2-3
+
+//1.10
+//var myApples = 17
+//var bobWants5ApplesFor3Oranges = 5
+//var bobWillGive3OrangesPer5Apples = 3
+//myApples/bobWants5ApplesFor3Oranges*bobWillGive3OrangesPer5Apples
+//myApples%5
+
+//1.11
+//var studentsOfTheClass = 80
+//var numberOfBoys = 20
+//var numberOfGirls = 60
+//var boyPercentage = numberOfBoys * 100/studentsOfTheClass
+//print(boyPercentage)
+//var girlPercentage = numberOfGirls * 100/studentsOfTheClass
+//print(girlPercentage)
+
+//2.1
+//var a = 11
+//var b = 22
+//if a > b {
+//    print(a)
+//} else {
+//    print(b)
+//}
+
+//2.2
+//var number = 2
+//if number % 2 == 0 {
+//print("\(number) is a even number")
+//} else {
+//    print("\(number) is a odd number")
+//}
+
+//2.3
+//var a = 12
+//var b = 3
+//
+//if a%b == 0 {
+//    print("\(a) is divisable by \(b)")
+//} else {
+//    print ("\(a) is not divisable by \(b)")
+//}
+
+//2.4
+//var a = 2
+//var b = 3
+//var c = 2
+//
+//if a == c || a == b || c == b  {
+//    print ("At least two of the variables are the same")
+//    
+//} else {
+//    print("Not two variables are the same")
+//    
+//}
+
+//2.5
+//var baconAge = 6
+//var eggAge = 12
+//
+//if baconAge <= 7 && eggAge <= 21 {
+//    print("Both the eggs and bacon are good to eat")
+//} else {
+//    print("Either the eggs or the bacon are bad")
+//}
+//
+//if baconAge > 7 {
+//    print("Throw out the bacon")
+//}
+//
+//if eggAge > 21 {
+//    print("Throw out the eggs")
+//}
+
+// 2.6
+//let year = 2014
+//
+//if year % 100 == 0 || year % 4 == 0 {
+//    print("Leap year")
+//} else {
+//    print("Not a leap year")
+//}
+
+//2.7
+//import Foundation
+//var randomNumber = arc4random()
+//if randomNumber % 2 == 0 {
+//    print("Heads")
+//} else {
+//    print("Tails")
+//}
+
+//2.8
+var a = 5
+var b = 6
+var c = 4
+var d = 3
+
+if 
+
+
+
+
+
+
+
